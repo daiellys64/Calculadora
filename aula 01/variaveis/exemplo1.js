55f0c31034c294d1955c5ef55006e1f545519c38
Daielly dos Santos <80227938+daiellys64@users.noreply.github.com>
2021-10-26 09:40:32 -0300

    p.a

---
+++ aula 01/variaveis/exemplo1.js
@@ -0,0 +1,8 @@
+var texto = "Vamos fazer uma calculadora" 
+var num1 = 25;
+var num2 = 26;
+soma = num1+num2
+alert(soma);
+// document.write(texto)
+ console.log(num1);
+document.getElementyById('teste').innerHTML = texto;
