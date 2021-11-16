6ede5a78601ff23717f4c620f6385d12a3358420
Daielly dos Santos <80227938+daiellys64@users.noreply.github.com>
2021-11-16 10:03:13 -0300

    Add files via upload

---
+++ aula02/js/meuscrispt.js
@@ -0,0 +1,14 @@
+function insere(numero){
+     var resultado= document.getElementById('resultado').innerHTML;
+     document.getElementsById('resultado').innerHTML =resultado+ numero;
+
+}
+function limpar(){
+    document.getElementsById('resultado').innerHTML= "";
+
+}
+ function voltar(){
+     var resultado= document.getElementById('resultado').innerHTML;
+                    document.getElementById('resultado').innerHTML=
+     document
+ }
